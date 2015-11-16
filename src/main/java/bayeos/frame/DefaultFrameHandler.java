b3a88ba2e2fe8dc95be0e3d9ed318c2a0a362c71
oarchner <oliver.archner@uni-bayreuth.de>
2015-11-16 18:27:48 +0100

    convenience methods added

--- src/main/java/bayeos/frame/DefaultFrameHandler.java
+++ src/main/java/bayeos/frame/DefaultFrameHandler.java
@@ -8,37 +8,34 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 public abstract class DefaultFrameHandler implements FrameHandler {
-	
-	
+
 	private String origin;
 	private Long timestamp;
-	private Integer rssi;	
-	private Map<String,SortedSet<Integer>> originMap = new HashMap<>();
-	
+	private Integer rssi;
+	private Map<String, SortedSet<Integer>> originMap = new HashMap<>();
+
 	private String defaultOrigin;
-	
-	public DefaultFrameHandler() {		
+
+	public DefaultFrameHandler() {
 		this.timestamp = new Date().getTime();
 		this.rssi = null;
 	}
-	
-	public DefaultFrameHandler(String defaultOrigin){
+
+	public DefaultFrameHandler(String defaultOrigin) {
 		this();
 		this.origin = defaultOrigin;
 		this.defaultOrigin = defaultOrigin;
 	}
-	
-	
-	public void resetHandler(){
-		this.origin = this.defaultOrigin; 
+
+	public void resetHandler() {
+		this.origin = this.defaultOrigin;
 		this.timestamp = new Date().getTime();
 	}
-		
 
-	// Calculated by handler 
+	// Calculated by handler
 	public String getOrigin() {
 		return origin;
-	}		
+	}
 
 	// Calculated by handler
 	public Date getTimeStamp() {
@@ -46,15 +43,14 @@ public abstract class DefaultFrameHandler implements FrameHandler {
 	}
 
 	// Calculated by handler
-	public Map<String, SortedSet<Integer>> getOriginMap(){		
+	public Map<String, SortedSet<Integer>> getOriginMap() {
 		return originMap;
 	}
 
-	
 	public Integer getRssi() {
 		return rssi;
 	}
-	
+
 	@Override
 	public void onDelay(long millis) {
 		timestamp = timestamp - millis;
@@ -62,95 +58,106 @@ public abstract class DefaultFrameHandler implements FrameHandler {
 
 	@Override
 	public void onOriginFrame(String origin) {
-		this.origin = origin;			    
+		this.origin = origin;
 	}
-	
-	
+
 	@Override
 	public void onRoute(int myId, int panId) {
 		StringBuilder b = new StringBuilder();
-		b.append(origin).append("/XBee").append(panId).append(":")
-				.append(myId);
-		origin = b.toString();			
+		b.append(origin).append("/XBee").append(panId).append(":").append(myId);
+		origin = b.toString();
 	};
-	
+
 	@Override
 	public void onRouteRssi(int myId, int panId, int rssi) {
 		onRoute(myId, panId);
-		this.rssi = rssi;		
+		this.rssi = rssi;
 	}
-	
+
 	@Override
 	public void onMillisecond(Date time) {
-		timestamp = time.getTime();				
-	} 
-	
-	
+		timestamp = time.getTime();
+	}
+
 	@Override
 	public void onTimeStamp(long time) {
-		this.timestamp = time;	
+		this.timestamp = time;
 	}
-	
+
 	@Override
 	public void onMessage(String message) {
 		addOriginKey();
+		onMessage(getOrigin(), getTimeStamp(), message);
 	}
-	
-	@Override
-	public void onCommand(byte type, byte[] value) {
-		addOriginKey();
-	}
-	
-	@Override
-	public void onDataFrame(byte type, Hashtable<Integer, Float> values) {
-		addOriginKey();						
-		
-		SortedSet<Integer> oldKeys = originMap.get(getOrigin());		
-		values.keySet().removeAll(oldKeys);
-		
-		if (values.keySet().size()>0){
-			originMap.get(getOrigin()).addAll(values.keySet());			
-			onNewChannels(getOrigin(), originMap.get(getOrigin()));
-		}		
-	}
-	
-	@Override
-	public void onDataFrameEnd() {
-		resetHandler();		
-	}
-	
-	
-	public void onNewOrigin(String origin){
-		
-		
-	}
-	
-	public void onNewChannels(String origin, SortedSet<Integer> channels){
-		
-	};
-	
 
-	private void addOriginKey() {
-		if (!originMap.containsKey(getOrigin())){
-			originMap.put(getOrigin(), new TreeSet<Integer>());	
-			onNewOrigin(getOrigin());
-		}
-	}
-	
-
-	
 	@Override
-	public void onError(String message) {	
+	public void onError(String message) {
 		addOriginKey();
+		onError(getOrigin(), getTimeStamp(), message);
 	}
 
 	@Override
 	public void onResponse(byte type, byte[] value) {
 		addOriginKey();
 	}
-	
-	
 
-			
-	
+	@Override
+	public void onCommand(byte type, byte[] value) {
+		addOriginKey();
+	}
+
+	@Override
+	public void onDataFrame(byte type, Hashtable<Integer, Float> values) {
+		addOriginKey();		
+		String origin = getOrigin();		
+		SortedSet<Integer> oldKeys = originMap.get(origin);		
+		SortedSet<Integer> newKeys = new TreeSet<Integer>();
+		
+		for(Integer channelNr: values.keySet()){
+			if (!oldKeys.contains(channelNr)){
+				newKeys.add(channelNr);				
+			}
+		}
+				
+		if (newKeys.size()>0){
+			originMap.get(origin).addAll(newKeys);
+			onNewChannels(origin,newKeys);
+		}
+						
+		onDataFrame(origin, getTimeStamp(), values, getRssi());
+		
+	}
+
+	@Override
+	public void onDataFrameEnd() {
+		resetHandler();
+	}
+
+	private void addOriginKey() {
+		if (!originMap.containsKey(getOrigin())) {
+			originMap.put(getOrigin(), new TreeSet<Integer>());
+			onNewOrigin(getOrigin());
+		}
+	}
+
+	public void onNewOrigin(String origin) {
+
+	}
+
+	public void onNewChannels(String origin, SortedSet<Integer> channels) {
+
+	}
+
+	public void onDataFrame(String origin, Date timeStamp, Hashtable<Integer, Float> values, Integer rssi) {
+
+	}
+
+	public void onMessage(String origin, Date timeStamp, String message) {
+
+	}
+
+	public void onError(String origin, Date timeStamp, String message) {
+
+	}
+
 }
