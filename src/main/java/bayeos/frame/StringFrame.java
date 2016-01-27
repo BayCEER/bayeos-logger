b65723206a5f17c47ec5892b586c82bbd4addf8e
oarchner <oliver.archner@uni-bayreuth.de>
2016-01-27 10:35:02 +0100

    Added StringFrame for Error and Messages

---
+++ src/main/java/bayeos/frame/StringFrame.java
@@ -0,0 +1,22 @@
+package bayeos.frame;
+
+import java.nio.ByteBuffer;
+
+public class StringFrame {
+	String value;
+	byte type;
+	
+	public StringFrame(byte type, String value) {
+			this.value = value;
+			this.type = type;
+	}
+	
+	public byte[] getBytes(){		
+		byte[] b = new byte[value.getBytes().length+1];
+		ByteBuffer bf = ByteBuffer.wrap(b);
+		bf.put(type);
+		bf.put(value.getBytes());
+		return b;		
+	}
+
+}
